d68aaf6faa86751b333714c34811f90bb256b278
rupesh <rupeshsaha899@gmail.com>
2025-02-28 16:35:45 +0530

    ui fix

--- src/components/TasksContainer.jsx
+++ src/components/TasksContainer.jsx
@@ -2,7 +2,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import TaskCard from "./TaskCard";
 import { useTasks } from "@/store/TaskContext";
-import { Loader2, Plus, PlusCircle } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import ConfigureTaskModal from "./ConfigureTaskModal";
 
 const TasksContainer = () => {
@@ -10,30 +10,51 @@ const TasksContainer = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [creating, setIsCreating] = useState(false)
+  const [creating, setIsCreating] = useState(false);
 
-const {fetchTasks, tasks} = useTasks()
+  const { fetchTasks, tasks } = useTasks();
 
   useEffect(() => {
-    try {
-      fetchTasks()
-      setLoading(true)
-    } catch (error) {
-      setError(error)
-    } finally {
-      setLoading(false)
-    }
-    
+    const loadTasks = async () => {
+      try {
+        setLoading(true);
+        await fetchTasks();
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadTasks();
   }, []);
 
   const filteredTasks =
     selectedCategory === "All"
       ? tasks
-      : tasks.filter((task) => task.category === selectedCategory.toLowerCase());
+      : tasks.filter(
+          (task) => task.category === selectedCategory.toLowerCase()
+        );
+
+  const getTaskCounts = (tasks) => {
+    const counts = { All: tasks.length, Work: 0, Study: 0, Personal: 0 };
+
+    tasks.forEach((task) => {
+      const category = task.category.toLowerCase();
+      const formattedCategory =
+        category.charAt(0).toUpperCase() + category.slice(1);
+      if (counts.hasOwnProperty(formattedCategory)) {
+        counts[formattedCategory]++;
+      }
+    });
+
+    return counts;
+  };
+
+  const taskCounts = getTaskCounts(tasks);
 
   return (
-    <div className=" min-h-[80vh] flex flex-col justify-start items-center p-4 gap-4">
-      <div className="flex md:justify-between justify-center items-center w-full py-2 px-3   gap-4">
+    <div className="min-h-[80vh] flex flex-col justify-start items-center p-4 gap-4">
+      <div className="flex md:justify-between justify-center items-center w-full py-2 px-3 gap-4">
         <div className="flex gap-4">
           {categories.map((category, idx) => (
             <div
@@ -43,9 +64,8 @@ const {fetchTasks, tasks} = useTasks()
               key={idx}
             >
               <button
-                key={idx}
                 onClick={() => setSelectedCategory(category)}
-                className={`flex cursor-pointer justify-center items-center duration-200  font-semibold transition  ${
+                className={`flex cursor-pointer justify-center items-center duration-200 font-semibold transition ${
                   selectedCategory === category
                     ? "text-blue-500"
                     : "text-gray-400 hover:text-blue-500"
@@ -54,18 +74,22 @@ const {fetchTasks, tasks} = useTasks()
                 {category}
               </button>
               <div
-                className={`p-3 flex justify-center items-center w-2 h-2 rounded-full text-white ${
+                className={`p-3 flex justify-center items-center w-6 h-6 rounded-full text-white text-sm  ${
                   selectedCategory === category
-                    ? "bg-blue-500 "
-                    : " bg-gray-400 hover:bg-blue-500"
+                    ? "bg-blue-500"
+                    : "bg-gray-400 hover:bg-blue-500"
                 }`}
               >
-                {filteredTasks.length}
+                {taskCounts[category]}
               </div>
+              
             </div>
           ))}
         </div>
-        <button onClick={()=>setIsCreating(true)} className=" bg-blue-500 text-white px-3 py-2 rounded-lg cursor-pointer hidden md:flex">
+        <button
+          onClick={() => setIsCreating(true)}
+          className="bg-blue-500 text-white px-3 py-2 rounded-lg cursor-pointer hidden md:flex"
+        >
           <Plus /> Add new task
         </button>
         <ConfigureTaskModal
